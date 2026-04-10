2a4a25f0f7f0b20b3f6543a8378b09ed1748b62e
jsaowji <39271213+jsaowji@users.noreply.github.com>
2026-04-10 19:06:26 +0200

    try fix windows a52dec compile; enable dvdread version check

--- libdvdread-sys/build.rs
+++ libdvdread-sys/build.rs
@@ -3,7 +3,7 @@ use pkg_config::Config;
 fn main() {
     println!("cargo::rerun-if-changed=build.rs");
     Config::new()
-        //.atleast_version("7.1.0")
+        .atleast_version("7.1.0")
         .probe("dvdread")
         .unwrap();
 }
