65dc1bea10c049f47eb88feca86f75d9a667cd12
jsaowji <39271213+jsaowji@users.noreply.github.com>
2026-05-10 10:56:11 +0200

    try link libc differently on musl

--- hatch_build.py
+++ hatch_build.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 from pathlib import Path
 from typing import Any
-
+import os
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from packaging import tags
 
@@ -14,8 +14,12 @@ class CustomHook(BuildHookInterface[Any]):
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         build_data["pure_python"] = False
-        build_data["tag"] = f"py3-none-{next(tags.platform_tags())}"
-        subprocess.run(["cargo", "build", "--release"], check=True)
+        ptag = next(tags.platform_tags())
+        envs = {}
+        if "musllinux" in ptag:
+            envs = { "RUSTFLAGS": "-C target-feature=-crt-static"}
+        build_data["tag"] = f"py3-none-{ptag}"
+        subprocess.run(["cargo", "build", "--release"], env=envs | os.environ, check=True)
         self.target_dir.mkdir(parents=True, exist_ok=True)
         for file_path in self.source_dir.glob("*"):
             if file_path.is_file() and file_path.suffix in [".dll", ".so", ".dylib"]:
